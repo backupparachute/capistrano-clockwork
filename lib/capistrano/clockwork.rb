5c08a78d703aba27d1449806892a5b81f52ab0d5
kyle <github@kylemiller.com>
2021-03-09 22:29:21 -0600

    bump version , stripped for debugging

--- lib/capistrano/clockwork.rb
+++ lib/capistrano/clockwork.rb
@@ -1,57 +1,57 @@
 require "capistrano/clockwork/version"
 
-module Capistrano
-  module DelayedJob
+# module Capistrano
+#   module DelayedJob
     
 namespace :clockwork do
   desc "Start clockwork"
   task :start do
     on roles(:app) do
-      on primary(:app) do
+      # on primary(:app) do
         # on release_roles(:all) do
-          # within release_path do
+          within release_path do
             with rails_env: fetch(:rails_env) do
               run "cd #{current_path} && RAILS_ENV=#{fetch(:rails_env)} bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log start"
               execute :bundle, :exec, :clockworkd, "-c lib/clock.rb --pid-dir tmp/pids --log --log-dir logs start"
             end
-          # end
+          end
         # end
-      end
+      # end
     end
   end
   
-  task :restart do
-    on roles(:app) do
-      on primary(:app) do 
-        on release_roles(:all) do
-          within release_path do
-          
-          with rails_env: fetch(:rails_env) do
-            # run "cd #{current_path} && RAILS_ENV=#{fetch(:rails_env)} bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log restart"
-              execute :bundle, :exec, :clockworkd, "-c lib/clock.rb --pid-dir tmp/pids --log --log-dir logs restart"
-          end
-        end
-        end
-      end
-    end
-  end
-  
-  task :stop do
-    on roles(:app) do
-      on primary(:app) do 
-        on release_roles(:all) do
-          within release_path do
-          
-          with rails_env: fetch(:rails_env) do
-            execute :bundle, :exec, :clockworkd, "-c lib/clock.rb --pid-dir tmp/pids --log --log-dir logs stop"
-            # run "cd #{current_path} && RAILS_ENV=#{fetch(:rails_env)} bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log stop"
-          end
-        end
-        end
-      end
-    end
-  end
-
-end
-end
+#   task :restart do
+#     on roles(:app) do
+#       on primary(:app) do
+#         on release_roles(:all) do
+#           within release_path do
+#
+#           with rails_env: fetch(:rails_env) do
+#             # run "cd #{current_path} && RAILS_ENV=#{fetch(:rails_env)} bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log restart"
+#               execute :bundle, :exec, :clockworkd, "-c lib/clock.rb --pid-dir tmp/pids --log --log-dir logs restart"
+#           end
+#         end
+#         end
+#       end
+#     end
+#   end
+#
+#   task :stop do
+#     on roles(:app) do
+#       on primary(:app) do
+#         on release_roles(:all) do
+#           within release_path do
+#
+#           with rails_env: fetch(:rails_env) do
+#             execute :bundle, :exec, :clockworkd, "-c lib/clock.rb --pid-dir tmp/pids --log --log-dir logs stop"
+#             # run "cd #{current_path} && RAILS_ENV=#{fetch(:rails_env)} bundle exec clockworkd -c #{current_path}/lib/clock.rb --pid-dir #{shared_path}/pids --log --log-dir #{shared_path}/log stop"
+#           end
+#         end
+#         end
+#       end
+#     end
+#   end
+#
+# end
+# end
 end
--- lib/capistrano/clockwork/version.rb
+++ lib/capistrano/clockwork/version.rb
@@ -1,5 +1,5 @@
 module Capistrano
   module Clockwork
-    VERSION = "0.3.11"
+    VERSION = "0.3.12"
   end
 end
